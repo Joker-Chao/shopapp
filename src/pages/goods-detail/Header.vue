33277ec2c9505de71461bada954a9c37c2050965
Joker Song <scw_99@163.com>
2020-04-30 17:03:15 +0800

    <feat>(localtion) : <正在完成商品详情>

---
+++ src/pages/goods-detail/Header.vue
@@ -0,0 +1,90 @@
+<template>
+	<div class="header-container">
+		<div v-if="showIconMenu" class="icon-header">
+			<span class="iconfont cell" @click="$router.back()">&#xe62f;</span>
+			<span class="iconfont cell" @click="$router.push('/cart')">&#xe60c;</span>
+		</div>
+		<div v-else class="menu-header border-bottom" :style="`opacity:${opacity}`">
+			<span class="iconfont cell" @click="$router.back()">&#xe62f;</span>
+			<ul class="header-tab">
+				<li :class="{active:scrollTab==='goods'}" @click="changeTab('goods')">商品</li>
+				<li :class="{active:scrollTab==='comment'}" @click="changeTab('comment')">评论</li>
+				<li :class="{active:scrollTab==='detail'}" @click="changeTab('detail')">详情</li>
+			</ul>
+			<span class="iconfont cell" @click="$router.push('/cart')">&#xe60c;</span>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default{
+		props:{
+			showIconMenu: {
+				type: Boolean,
+				default: true
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+@import "~@/assets/scss/global";
+.header-container{
+  width: 100%;
+  height: .88rem;
+  position: fixed;
+  left: 0;
+  top: 0;
+  z-index: 100;
+  .icon-header{
+    width: 100%;
+    height: 100%;
+    padding: 0 .2rem;
+    box-sizing: border-box;
+    @include layout-flex($justify: space-between);
+    background: linear-gradient(rgba(0,0,0,.15),transparent);
+    .cell{
+      width: .6rem;
+      height: .6rem;
+      border-radius: 50%;
+      background: rgba($color: #000000, $alpha: .3);
+      @include layout-flex;
+      color: $color-white;
+      font-size: .4rem;
+    }
+  }
+  .menu-header{
+    width: 100%;
+    height: 100%;
+    padding: 0 .2rem;
+    box-sizing: border-box;
+    @include layout-flex($justify: space-between);
+    background: $color-white;
+    color: $color-six;
+    .cell{
+      width: .6rem;
+      height: .6rem;
+      @include layout-flex;
+      font-size: .4rem;
+    }
+    .header-tab{
+      width: 0;
+      flex: 1;
+      margin: 0 .6rem;
+      height: 100%;
+      @include layout-flex;
+      li{
+        width: 33.33%;
+        height: 100%;
+        @include layout-flex;
+        border-bottom: 2px solid $color-white;
+        box-sizing: border-box;
+        &.active{
+          color: $color-global;
+          border-bottom: 2px solid $color-global;
+        }
+      }
+    }
+  }
+}
+</style>
--- src/pages/goods-detail/index.vue
+++ src/pages/goods-detail/index.vue
@@ -1,17 +1,20 @@
 <template>
-	<div>sadfdfas</div>
+	<div>
+		<detail-header></detail-header>
+	</div>
 </template>
 
 <script>
+	import DetailHeader from './Header'
 	export default{
 		props:{
 			id: Number
 		},
 		data(){
 			return {
-				// comment: {},
-				// gallery:[],
-				// goods: {}
+				comment: {},
+				gallery:[],
+				goods: {}
 			}
 		},
 		mounted(){
@@ -20,13 +23,13 @@
 		methods:{
 			getGoods(){
 				this.axios.get(`api/goods?goods_id=${this.id}`).then(result => {
-					// const {comment:commentList,commentTotal,gallery,goods} = result
-					// this.comment = {
-					// 	list: commentList,
-					// 	count: commentTotal,
-					// }
-					// this.gallery = gallery
-					// this.goods = goods
+					const {comment:commentList,commentTotal,gallery,goods} = result
+					this.comment = {
+						list: commentList,
+						count: commentTotal,
+					}
+					this.gallery = gallery
+					this.goods = goods
 					console.log(result)
 				}).catch(err => {
 					console.log(err)
